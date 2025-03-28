d3ab7160f07094f6c794094c85d1981c08306111
jpsilvadev <jpsjoaosilva98@gmail.com>
2025-03-28 14:39:58 +0000

    serve files

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jpsilvadev/goserve
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+
+	mux.HandleFunc("/healthz", handlerReadiness)
+
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
